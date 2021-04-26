1c0ad630d6f0866b6e41abc77de7cd0adf285dd7
Javier David Banda Villeda <jbandav@itsoeh.edu.mx>
2021-04-26 13:38:31 -0500

    Transiciones en landing

--- atexcare/demo/static/demo/js/background.js
+++ atexcare/demo/static/demo/js/background.js
@@ -37,4 +37,34 @@ $(function () {
             $('#btn-plus i').css('transform', 'rotate(0deg)')
         }
     })
+
+
+    var swiper = new Swiper('.swiper-container2', {
+        effect: 'cube',
+        grabCursor: true,
+        cubeEffect: {
+            shadow: true,
+            slideShadows: true,
+            shadowOffset: 20,
+            shadowScale: 0.94,
+        },
+        loop: true,
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+        },
+        autoplay: {
+            delay: 4000,
+        },
+    });
+
+    var sw = new Swiper('.swiper-container', {
+        spaceBetween: 30,
+        effect: 'fade',
+        loop: true,
+        autoplay: {
+            delay: 3000,
+        },
+    });
+
 })
